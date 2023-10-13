52a2b51efae9f6576e471f307ddc8c7807338bb8
Robertas <robertas.palinskis@baltotrader.com>
2023-10-13 14:17:41 +0300

    request

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:rust_in_flutter/rust_in_flutter.dart';
-import 'package:flicon/messages/report_in.pb.dart' as  report_in;
-import 'package:example_app/messages/device_info.pb.dart'
-    as deviceInfo;
+import 'package:flicon/messages/device_info.pb.dart' as deviceInfo;
 
 void main() async {
   // Wait for Rust initialization to be completed first.
@@ -26,6 +24,7 @@ class _MyAppState extends State<MyApp> {
       return AppExitResponse.exit;
     },
   );
+
 
   @override
   void dispose() {
@@ -55,42 +54,38 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  Future<void> btn() async {
+    var requestMessage = deviceInfo.ReadRequest(
+      inputNumbers: [1],
+      inputString: 'n',
+    );
+    var rustResponse = await requestToRust(RustRequest(
+      resource: deviceInfo.ID,
+      operation: RustOperation.Read,
+      message: requestMessage.writeToBuffer(),
+    ));
+    var responseMessage =
+        deviceInfo.ReadResponse.fromBuffer(
+          rustResponse.message!,
+        );
+    print(responseMessage.outputNumbers);
+    print(responseMessage.outputString);
+  }
+
   @override
   Widget build(BuildContext context) {
+    deviceInfo.ReadRequest requestMessage;
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // `StreamBuilder` listens to a stream
-            // and rebuilds the widget accordingly.
-            StreamBuilder<RustSignal>(
-              // Receive signals from Rust
-              // with `rustBroadcaster` from `rust_in_flutter.dart`,
-              // For better performance, filter signals
-              // by checking the `resource` field with the `where` method.
-              // This approach allows the builder to rebuild its widget
-              // only when there are signals
-              // related to a specific Rust resource it is interested in.
-              stream: rustBroadcaster.stream.where((rustSignal) {
-                return rustSignal.resource == report_in.ID;
-              }),
-              builder: (context, snapshot) {
-                print(snapshot.error.toString());
-                print(snapshot.data.toString());
-                // If the app has just started and widget is built
-                // without receiving a Rust signal,
-                // the snapshot's data will be null.
-                final rustSignal = snapshot.data;
-                if (rustSignal == null) {
-                  // Return a black container if the received data is null.
-                  return Text(snapshot.data.toString());
-                } else {
-                  // Return an image container if some data is received.
-                  return Text(snapshot.data.toString());
-                }
-              },
-            ),
+          
+            ElevatedButton(
+              onPressed: btn,
+              child: const Text("Request to Rust")
+            )
           ],
         ),
       ),
