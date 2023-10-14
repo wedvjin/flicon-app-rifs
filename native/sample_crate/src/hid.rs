79fc74bdd3a56822d864cb3e05104d7a42ee810f
rust <cived@protonmail.com>
2023-10-14 20:51:09 +0300

    send feature instead of write

--- native/sample_crate/src/hid.rs
+++ native/sample_crate/src/hid.rs
@@ -186,6 +186,7 @@ impl DeviceState {
 
     pub fn send_feature(&self) {
         let mut buf: [u8; 128] = [0; 128];
+        buf[0] = 3;
         self.feature.to_bytes(&mut buf);
         self.device.send_feature_report(&buf).unwrap();
     }
