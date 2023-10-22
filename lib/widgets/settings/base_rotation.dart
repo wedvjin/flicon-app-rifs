ef20b97ce57c4bf104ab010de90d004b92d14aff
Robertas <robertas.palinskis@baltotrader.com>
2023-10-22 16:05:56 +0300

    z axis calibration

--- lib/widgets/settings/base_rotation.dart
+++ lib/widgets/settings/base_rotation.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_multi_slider/flutter_multi_slider.dart';
 import 'package:flicon/messages/report_message.pb.dart' as reportMessage;
-
+import 'package:rust_in_flutter/rust_in_flutter.dart';
+import 'package:flicon/messages/device_info.pb.dart' as deviceInfo;
 class BaseRotation extends StatefulWidget {
   final reportMessage.ReportMessage data;
 
@@ -13,27 +16,63 @@ class BaseRotation extends StatefulWidget {
 
 class _BaseRotationState extends State<BaseRotation> {
 
-  final double _rotationValue = 0.0;
-  final double _realRotationValue = 0.0;
-  
-  double _centerPostion = 0.0;
+  late Timer _periodicZTimer;
 
   bool _showCalibation = false;
+  bool rAxisCalibration = false;
 
-  List<double> calibration = [0, 0.05, 0.05, 0.1];
-  final double _radiusValue = 0.0;
-  final bool _negativeValue = false;
+  // Z VALUES
+
+  var minZValue = 10000000000; 
+  var maxZValue = -10000000000; 
+  double _currentZMin = 0;
+  bool minZChanged = false;
+  double _currentZMax= 0;
+  bool maxZChanged = false;
+  bool deadZoneZChanged = false;
+  double _editableDeadZoneZValue = 0;
+  bool averageZChanged = false;
+  double _editableZAverage = 0;
+  bool rZAxisCalibration = false;
+
+
+  bool callbackMessage = false;
+
+  bool _negativeValue = false;
+
+  Future<deviceInfo.ReadResponse> rust_request(message, value1, value2, value3, value4, RustOperation operation) async {
+    final requestMessage = deviceInfo.SetValues(
+      target: message,
+      value1: value1,
+      value2: value2,
+      value3: value3,
+      value4: value4,
+    );
+    var rustResponse = await requestToRust(RustRequest(
+      resource: deviceInfo.ID,
+      operation: operation,
+      message: requestMessage.writeToBuffer(),
+    ));
+    var responseMessage =
+        deviceInfo.ReadResponse.fromBuffer(
+          rustResponse.message!,
+        );
+    return responseMessage;
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
 
-    double rotationAngle = (widget.data.z * 100 / 32768) - 50;
+    double rotationAngle = (widget.data.zAxis * 100 / widget.data.zMax) / 4;
 
     
     return Column(
       children: [
         Transform.rotate(
-          angle: _centerPostion,
+          angle: 0,
           child: Stack(
             children: <Widget>[
               Container(
@@ -126,7 +165,21 @@ class _BaseRotationState extends State<BaseRotation> {
             ],
           ),
         ),
-      
+
+        if(callbackMessage)
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                child: Container(
+              width: 300,
+              height: 40,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                  color: Color.fromRGBO(2, 42, 22, 1),
+                  shape: BoxShape.rectangle,
+                ),
+              child: Text('Settings sent do device.'),
+            )),
+    
         Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
                 child: ElevatedButton(
@@ -144,55 +197,237 @@ class _BaseRotationState extends State<BaseRotation> {
         if(_showCalibation) 
           Column(
             children: [
+              const Divider(
+                color: Color.fromRGBO(41, 41, 41, 1)
+              ),
+              const Padding(
+                padding: EdgeInsets.all(5),
+                child: Text("Z Axis"),
+              ),
+              Stack(
+                children: <Widget>[
+                  Container(
+                    width: 250,
+                    height: 10,
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    child: Container(
+                      width: 250,
+                      height: 10,
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(169, 193, 10, 1),
+                        shape: BoxShape.rectangle,
+
+                      ),
+                    ),
+                  ),
+                  if(rZAxisCalibration)
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      child: Container(
+                        width: (((widget.data.xAxis - minZValue) / (maxZValue - minZValue)) * 250).clamp(0, 250),
+                        height: 10,
+                        decoration: const BoxDecoration(
+                          color: Color.fromRGBO(96, 110, 3, 1),
+                          shape: BoxShape.rectangle,
+
+                          ),
+                        ),
+                      ),
+                  if(rZAxisCalibration)
+                    Positioned(
+                      top: 0,
+                      left: (((widget.data.xAxis - minZValue) / (maxZValue - minZValue)) * 250).clamp(0, 250) - (250 * (deadZoneZChanged ? _editableDeadZoneZValue.toDouble() : widget.data.zDeadZone.toDouble()) / 100 / 2),
+                      child: Container(
+                        width: 250 * (deadZoneZChanged ? _editableDeadZoneZValue.toDouble() : widget.data.zDeadZone.toDouble()) / 100,
+                        height: 10,
+                        decoration: const BoxDecoration(
+                          color:Color.fromRGBO(190, 4, 4, 0.8),
+                          shape: BoxShape.rectangle,
+                        ),
+                      ),
+                    ),
+                  if(!rZAxisCalibration)
+                    Positioned(
+                      top: 0,
+                      left: 125 - (250 * (deadZoneZChanged ? _editableDeadZoneZValue.toDouble() : widget.data.zDeadZone.toDouble()) / 100 / 2),
+                      child: Container(
+                        width: 250 * (deadZoneZChanged ? _editableDeadZoneZValue.toDouble() : widget.data.zDeadZone.toDouble()) / 100,
+                        height: 10,
+                        decoration: const BoxDecoration(
+                          color:Color.fromRGBO(190, 4, 4, 0.8),
+                          shape: BoxShape.rectangle,
+                        ),
+                      ),
+                    ),
+                    
+                ],
+              ),
+              if(rZAxisCalibration)
+                const Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Text(
+                    "Rotate left and right and hold few seconds",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              if(rZAxisCalibration)
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 5),
+                  child: Text(
+                    "to set Z axis MIN and MAX values",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20) , 
-                child: MultiSlider(
-                  min: 0.00,
-                  max: 0.1,
-                  height: 50,
-                  horizontalPadding: 50,
-                  activeTrackSize: 2,
-                  inactiveTrackSize: 2,
-                  textHeightOffset: -20,
-                  thumbRadius: 10,
-                  values: calibration,
-                  thumbColor: const Color.fromRGBO(193, 10, 10, 1),
-                  color: const Color.fromRGBO(193, 10, 10, 1),
-                  textDirection: TextDirection.rtl,
-                  onChanged: (value) => {
+                padding: EdgeInsets.all(5),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero),
+                    backgroundColor: const Color.fromARGB(255, 62, 62, 62),
+                    foregroundColor: Colors.white),
+                  child: Text('${rZAxisCalibration ? 'End': 'Start'} calibration'),
+                  onPressed: () {
                     setState(() {
-                      calibration = value;
-                    })
+                      rZAxisCalibration = !rZAxisCalibration;
+                      if(rZAxisCalibration) {
+                        minZChanged = true;
+                        maxZChanged = true;
+
+                        var valueInZRange = widget.data.zAxis;
+                        minZValue = valueInZRange < minZValue ? valueInZRange : minZValue;
+                        maxZValue = valueInZRange > maxZValue ? valueInZRange : maxZValue;
+
+                        _periodicZTimer = Timer.periodic(Duration(milliseconds: 40), (timer) { 
+                          var valueInZRange = widget.data.zAxis;
+                          minZValue = valueInZRange < minZValue ? valueInZRange : minZValue;
+                          maxZValue = valueInZRange > maxZValue ? valueInZRange : maxZValue;
+                        });
+                      } else {
+                        if (_periodicZTimer.isActive) {
+                          _periodicZTimer.cancel();
+                        }
+                      }
+                    });
                   },
-                  divisions: 48,
                 )
               ),
-              Text('Center average : ${(_centerPostion * 100).round()}'),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                padding: const EdgeInsets.only(top: 5),
+                child: Text('Dead zone : ${deadZoneZChanged ? _editableDeadZoneZValue.toInt() : widget.data.zDeadZone.toInt()}%'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5, bottom: 5),
                 child: Slider(
-                  value: _centerPostion.toDouble(), 
-                  min: -1,
-                  max: 1,
-                  divisions: 100,
+                  value: deadZoneZChanged ? _editableDeadZoneZValue : widget.data.zDeadZone.toDouble(), 
+                  min: 1,
+                  max: 50,
+                  divisions: 50,
                   activeColor:const Color.fromRGBO(193, 10, 10, 1)  ,
                   onChanged: (value) => {
-                    setState(() => _centerPostion = value)
+                    setState(() {
+                      if(!deadZoneZChanged) {
+                        deadZoneZChanged = true;
+                      }
+                      _editableDeadZoneZValue = value;
+                    })
                   }
                 )
               ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero),
-                    backgroundColor: const Color.fromARGB(255, 62, 62, 62),
-                    foregroundColor: Colors.white),
-                child: const Text('Apply & Save'),
-                onPressed: () => {
-                  setState(() => _showCalibation = !_showCalibation)
-              
-                },
+              Padding(
+                padding: const EdgeInsets.only(top: 0),
+                child: Text('Averaging: ${averageZChanged ? _editableZAverage.toInt() : widget.data.zAveraging.toInt()}'),
               ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5, bottom: 5),
+                child: Slider(
+                  value: averageZChanged ? _editableZAverage : widget.data.zAveraging.toDouble(), 
+                  min: 1,
+                  max: 300,
+                  divisions: 300,
+                  activeColor:const Color.fromRGBO(193, 10, 10, 1)  ,
+                  onChanged: (value) => {
+                    setState(() {
+                      if(!averageZChanged) {
+                        averageZChanged = true;
+                      }
+                      _editableZAverage = value;
+                    })
+                  }
+                )
+              ),
+            
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: 
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero),
+                        backgroundColor: const Color.fromARGB(255, 62, 62, 62),
+                        foregroundColor: Colors.white),
+                    child: const Text('Apply & Save'),
+                    onPressed: () {
+                      int minZRange = 0;
+                      int maxZRange = 0;
+                      int averageZ = 0;
+                      int deadZoneZ = 0;
+
+                      if(deadZoneZChanged) {
+                        deadZoneZ = _editableDeadZoneZValue.toInt();
+                        setState(() {
+                          deadZoneZChanged = false;
+                        });
+                      } else {
+                        deadZoneZ = widget.data.zDeadZone;
+                      }
+
+                      if(averageZChanged) {
+                        averageZ = _editableZAverage.toInt();
+                        setState(() {
+                          averageZChanged = false;
+                        });
+                      } else {
+                        averageZ = widget.data.zAveraging;
+                      }
+
+                      if(minZChanged) {
+                        minZRange = minZValue;
+                        setState(() {
+                          minZChanged = false;
+                        });
+                      } else {
+                        minZRange = widget.data.zMin;
+                      }
+
+                      if(maxZChanged) {
+                        maxZRange = maxZValue;
+                        setState(() {
+                          maxZChanged = false;
+                        });
+                      } else {
+                        maxZRange = widget.data.zMax;
+                      }
+
+                      rust_request('setz', minZRange, maxZRange, averageZ, deadZoneZ, RustOperation.Update);
+                      rust_request('apply', 0, 0, 0, 0, RustOperation.Update);
+                      rust_request('save', 0, 0, 0, 0, RustOperation.Update);
+
+                      _showCalibation = false;
+                      rZAxisCalibration = false;
+
+                      callbackMessage = true;
+
+                      Future.delayed(const Duration(seconds: 5)).then((value) => callbackMessage = false);
+                      
+                    },
+            
+                  ),
+              )
             ]
           )
 
