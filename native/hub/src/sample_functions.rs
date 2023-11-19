35dfcda17e9906c3d16b29802f0e6fbd520b9896
rust <cived@protonmail.com>
2023-11-19 15:34:21 +0200

    push

--- native/hub/src/sample_functions.rs
+++ native/hub/src/sample_functions.rs
@@ -123,7 +123,7 @@ pub async fn stream_report(
     use sample_crate::Buttons;
 
     loop {
-        crate::sleep(std::time::Duration::from_millis(1)).await;
+        crate::sleep(std::time::Duration::from_millis(10)).await;
         // TODO: REMAKE IT IS A QUICK WORKAROUND
         let mut report_in_data: ReportIn = ReportIn {
             ..Default::default()
